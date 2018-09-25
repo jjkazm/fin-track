66ddc6b25e953a7318ac8f6e9b886d527dbdd608
jjkazm <jjkazm@gmail.com>
2018-09-25 17:47:56 +0200

    Revert "add bootstrap 4"
    This reverts commit 4c8efc2a2cfe15773384c7a4e0e41ce657ce4960.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
