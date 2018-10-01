5f214f0eb8e0ae4b8a2bfc59b45b082b145a5e67
jjkazm <jjkazm@gmail.com>
2018-10-01 23:59:24 +0200

    refactor stock controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,22 +1,13 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_search(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js {render partial:'users/result'}
-        end
-      else
-        respond_to do |format|
-          flash.now[:danger] = "Provided code is invalid"
-          format.js {render partial:'users/result'}
-        end
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You need to provide company's code"
     else
-      respond_to do |format|
-        flash.now[:danger] = "You need to provide company's code"
-        format.js {render partial:'users/result'}
-      end
+      @stock = Stock.new_from_search(params[:stock])
+          flash.now[:danger] = "Provided code is invalid" unless @stock
+    end
+    respond_to do |format|
+      format.js {render partial:'users/result'}
     end
   end
 end
