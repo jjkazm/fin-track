67cc3a27b730ef06b5c1f8264efe7b2f636e48c3
jjkazm <jjkazm@gmail.com>
2018-10-02 22:50:37 +0200

    add limit of stocks

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def already_added?(ticker)
+    stock = Stock.find_by_ticker(ticker)
+    return false unless stock
+    stocks.where(ticker: ticker).exists?
+  end
+
+  def above_limit?
+    stocks.count>=10
+  end
+
+  def can_be_added?(ticker)
+    return true  unless (already_added?(ticker) || above_limit?)
+  end
 end
